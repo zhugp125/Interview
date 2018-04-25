7aadf49244576a93e668a56681f2a7e4a45967e0
zhugp <18811475054@163.com>
2018-04-25 22:32:43 +0800

    add dir

--- object_type.py
+++ object_type.py
@@ -23,11 +23,40 @@ class Animal(object):
 print(type(Animal())) # <type '__main__.Animal'>
 
 # type return value compare
-print(type(123) == int)
-print(type(123) == type(456))
-print(type('123') == str)
-print(type(123) == type('123'))
-print(type(abs) == types.BuiltinFunctionType)
-print(type(fn) == types.FunctionType)
-print(type(lambda x: x) == types.LambdaType)
-print(type((x for x in range(10))) == types.GeneratorType)
+print(type(123) == int) # True
+print(type(123) == type(456)) # True
+print(type('123') == str) # True
+print(type(123) == type('123')) # False
+print(type(abs) == types.BuiltinFunctionType) # True
+print(type(fn) == types.FunctionType) # True
+print(type(lambda x: x) == types.LambdaType) # True
+print(type((x for x in range(10))) == types.GeneratorType) # True
+
+# isinstance(obj, objectName)
+class Animal(object):
+    pass
+
+class Dog(Animal):
+    pass
+
+class Husky(Dog):
+    pass
+
+a = Animal()
+d = Dog()
+h = Husky()
+
+# sub object is also a base object
+print(isinstance(h, Husky)) # True
+print(isinstance(h, Dog)) # True
+print(isinstance(h, Animal)) # True
+
+# base object is not a sub object
+print(isinstance(d, Husky)) # False
+
+# dir: get all attrtitue or method for a object
+print(dir('abc'))
+
+# setattr
+# getattr
+# hasattrsss

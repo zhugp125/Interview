ac29b3704db55434244edeafa27662efa09a2388
朱高鹏 <33352915+zhugp125@users.noreply.github.com>
2018-08-03 16:44:05 +0800

    java
    delete file test

---
+++ DelFile.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class DelFile {
+    static void deleteFile(File file) {
+        if (!file.exists()) {
+            return ;
+        }
+
+        if (file.isFile()) {
+            file.delete();
+        } else {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                deleteFile(f);
+            }
+            file.delete();
+        }
+    }
+
+    public static void main(String[] args) {
+        deleteFile(new File("test"));
+    }
+}

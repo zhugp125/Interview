95bd8932cd1effd4a0ab0b1bf55b6505e298f8bb
朱高鹏 <33352915+zhugp125@users.noreply.github.com>
2018-10-29 19:53:03 +0800

    单例

---
+++ singleton/CSingletonT.h
@@ -0,0 +1,33 @@
+﻿/**
+ *  单体模板
+ *
+ *  @author zhangyi
+ *  @since  2012-06-05
+ */
+
+#ifndef SERVER3_BASE_UTILS_TTSINGLETONT_H
+#define SERVER3_BASE_UTILS_TTSINGLETONT_H
+
+#include "utils/utilsCommon.h"
+#include <boost/thread/recursive_mutex.hpp>
+
+#include <boost/thread/recursive_mutex.hpp>
+
+namespace utils
+{
+    template <class T>
+    class CSingletonT
+    {
+    public:
+        static T *instance();
+
+        static void release();
+
+    protected:
+
+        static T *s_pInstance;
+    };
+
+}
+
+#endif
---
+++ singleton/CSingletonT.hpp
@@ -0,0 +1,38 @@
+﻿/**
+ *  单体模板
+ *
+ *  @author zhangyi
+ *  @since  2012-06-05
+ */
+
+#include <utils/CSingletonT.h>
+
+namespace utils
+{
+    template <class T>
+    T * CSingletonT<T>::instance()
+    {
+        if (NULL == s_pInstance)
+        {
+            s_pInstance = new T();
+        }
+        return s_pInstance;
+    }
+
+    template <class T>
+    void CSingletonT<T>::release()
+    {
+        if (NULL != s_pInstance)
+        {
+            delete s_pInstance;
+            s_pInstance = NULL;
+        }
+    }
+
+#define IMPL_SINGLETON_CLASS(subClass)  \
+    namespace utils \
+    {   \
+        template<> subClass * CSingletonT<subClass>::s_pInstance = NULL;   \
+    }\
+    template class utils::CSingletonT<subClass>
+}
